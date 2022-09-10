1132c582523252059cdb67a34f5361f326b1e058
Urvi Goel <urvigoel26@gmail.com>
2022-09-10 11:21:04 +0530

    created react-app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hey, are you enjoying learning cloud computing?
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
